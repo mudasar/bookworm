5d003f1461ccc17dd3f20630d5d82aea86e958ef
Mudasar Rauf <mudasar.rauf@latra.com>
2019-02-21 12:29:10 +0000

    React router setup with couple of pages

---
+++ src/components/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import LoginPage from './LoginPage';
+
+const HomePage = () => (
+  <div>
+    <h1>Home Page</h1>
+    <Link to="/login" exact component={LoginPage}>Login</Link>
+  </div>
+);
+
+export default HomePage;
